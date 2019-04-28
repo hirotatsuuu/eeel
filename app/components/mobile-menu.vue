006e2d4c11892518195b5792657d810ea7c818d1
hirotatsu <hirotatsuuu@gmail.com>
2019-04-29 01:27:54 +1200

    create mobile-menu

---
+++ app/components/mobile-menu.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <v-speed-dial
+      v-model="fab"
+      bottom
+      left
+      hover
+      fixed
+    >
+      <template v-slot:activator>
+        <v-btn
+          v-model="fab"
+          white
+          fab
+        >
+          <v-icon>menu</v-icon>
+          <v-icon>close</v-icon>
+        </v-btn>
+      </template>
+      <v-btn
+        fab
+        dark
+        color="green"
+        v-on:click="gotoMyAccount"
+      >
+        <v-icon>person</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="indigo"
+        v-on:click="gotoHome"
+      >
+        <v-icon>home</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="purple"
+      >
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="yellow"
+      >
+        <v-icon>notifications</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="red"
+      >
+        <v-icon>mail</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        v-on:click="gotoSettings"
+      >
+        <v-icon>settings</v-icon>
+      </v-btn>
+    </v-speed-dial>
+    <v-btn
+      dark
+      fab
+      bottom
+      right
+      fixed
+      v-on:click="gotoCreatePost"
+    >
+      <v-icon>add</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import firebase from '~/plugins/firebase'
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
+
+export default {
+  middleware: 'authenticated',
+  data() {
+    return {
+      user: this.$store.getters['user/user']
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getUser: ['user/user']
+    }),
+  },
+  created () {
+    console.log('header created', this.user)
+    this.$store.subscribe((mutation, state) => {
+      this.user = this.getUser
+    })
+  },
+  methods: {
+    gotoHome() {
+      this.$router.push("/")
+    },
+    gotoCreatePost() {
+      this.$router.push("/posts/create")
+    },
+    gotoMyAccount () {
+      console.log('gotoMyAccount', this.user)
+      this.$router.push("/users/" + this.user.uid)
+    },
+    gotoSettings() {
+      this.$router.push("/settings")
+    }
+  }
+}
+</script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,23 +1,54 @@
 <template>
   <v-app>
-    <Header />
+    <div v-if="width > 825">
+      <Header />
+    </div>
+    <div v-else>
+      <MobileMenu />
+    </div>
     <v-content>
       <v-container class="pt-0 mt-1">
         <nuxt />
       </v-container>
     </v-content>
-    <Footer />
+    <div v-if="width > 825">
+      <Footer />
+    </div>
+    <div v-else>
+      <v-footer height="80">
+        <v-container>
+          <v-layout justify-center>
+            <img src="@/assets/images/logo.png" alt="logo" style="height: 35px;" v-on:click="gotoHome()">
+          </v-layout>
+        </v-container>
+      </v-footer>
+    </div>
   </v-app>
 </template>
 
 <script>
 import Header from '@/components/header'
 import Footer from '@/components/footer'
+import MobileMenu from '@/components/mobile-menu'
 
 export default {
+  data() {
+    return {
+      width: window.parent.screen.width
+    }
+  },
   components : {
     Header,
-    Footer
-  }
+    Footer,
+    MobileMenu
+  },
+  created() {
+    console.log('window.parent.screen.width', )
+  },
+  methods: {
+      gotoHome() {
+        this.$router.push("/")
+      }
+    }
 }
 </script>
